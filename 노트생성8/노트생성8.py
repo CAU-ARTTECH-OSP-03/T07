d9a5ee695cf4a0d9bdf5a790e1764a760db9ed8b
HAN-GISU <hks4939@naver.com>
2021-11-21 12:23:07 +0900

    수정

--- 노트생성8/노트생성8.py
+++ 노트생성8/노트생성8.py
@@ -4,23 +4,24 @@ import random
 
 #변수
 play = True
-SCREENWIDTH = 800
 SCREENHEIGHT = 400
+SCREENWIDTH = 800
 move = Rect(0,0,0,0)
 time1000ms = 0
 
 #스크린생성
 pygame.init()
+
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('노트랜덤생성')
 
 #시간def
 clock = pygame.time.Clock()
 
-def timeUpdate1000ms(): # 노트생성 딜레이
+def timeUpdate1000ms():
     global time1000ms
     time1000ms += 1
-    if time1000ms > 5:
+    if time1000ms > 100: # 노트생성 딜레이
         time1000ms = 0
         return True
     return False
@@ -30,26 +31,28 @@ def timeUpdate1000ms(): # 노트생성 딜레이
 
 def makeNote():
     if timeUpdate1000ms():
-        idex = random.randint(0, len(note)-1)
-        if rectNote[idex].x == -1:
-            rectNote[idex].y = random.randint(0, SCREENHEIGHT - 100)
-            rectNote[idex].x = 0 #x가 0일때 시작하기
+        index = random.randint(0, len(note)-1)
+        if rectNote[index].x == -1:
+            rectNote[index].y = random.randint(0, SCREENHEIGHT - 100)
+            rectNote[index].x = 0 #x가 0일때 시작하기
 #노트이동def
 def moveNote():
+
     makeNote()
-
     for i in range(len(note)):
-        if rectNote[i].x == -1:
-            continue
-
-        rectNote[i].x += 5 #노트속도
         if rectNote[i].x > SCREENWIDTH:
             rectNote[i].x = -30 #오른쪽 끝으로 넘어가면 노트가 다시 x좌표 -30 부터 시작
+        if rectNote[i].x == -1:
+            continue
+        rectNote[i].x += 5 #노트속도
+
         SCREEN.blit(note[i], rectNote[i])
 
 #노트생성
-note = [pygame.image.load('Deemo_long.png') for i in range(40)] # 노트 수
+note = [pygame.image.load('Deemo_long.png') for i in range(33)] # 노트 수
+
 rectNote = [None for i in range(len(note))]
+
 for i in range(len(note)):
     note[i] = pygame.transform.scale(note[i], (30, 100))
     rectNote[i] = note[i].get_rect()
@@ -66,6 +69,6 @@ while play:
     moveNote()
 
     pygame.display.flip()
-    clock.tick(100)    
+    clock.tick(60)    
 
 pygame.quit()
