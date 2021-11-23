c9a4126d38fd8cfc5fc7a640a7b44ca7a325c9ab
HAN-GISU <hks4939@naver.com>
2021-11-23 18:54:55 +0900

    통합완료

--- 통합시도/통합시도.py
+++ 통합시도/통합시도.py
@@ -8,6 +8,18 @@ SCREENHEIGHT = 400
 SCREENWIDTH = 800
 move = Rect(0,0,0,0)
 delay = 0
+
+#바 생성
+bar=pygame.image.load("bar.png")  #원본캐릭터 사진 업로드  원하는 경로의 사진을 복사 붙여넣기
+bar_width,bar_height=30,80           #캐릭터 가로,세로  설정   가로:60  세로:120
+real_char=pygame.transform.scale(bar,(30,80))   #원본캐릭터에서에서 원하는 크기로 커스텀
+xpos= 720    #바의 x좌표위치
+ypos= SCREENHEIGHT/2- bar_height/2 #바의 y좌표위치
+to_y = 0   #바가 y 좌표로 이동하는 정도
+bar_speed=0.5 #바의 속도
+
+
+
 #노트생성
 note = [pygame.image.load('Deemo_long.png') for i in range(33)] # 노트 수
 rectNote = [None for i in range(len(note))]
@@ -23,10 +35,9 @@ line = pygame.image.load("line.png")
 line_x_pos = 740
 line_y_pos = 0
 
+
 #시간def
 clock = pygame.time.Clock()
-
-
 def delayUpdate(): #노트생성 딜레이 def
     global delay  
     delay += 1
@@ -70,10 +81,24 @@ for i in range(len(note)): #노트생성을 pygame 기본 rect에 저장
 #while문
 
 while play:
+    dt=clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             play = False
+        if event.type ==pygame.MOUSEBUTTONDOWN: #마우스가 눌렸는지
+            if event.button == 4:   #마우스휠 올리기
+                to_y = -bar_speed #캐릭터를 위로 캐릭터의 속도만큼 올리기
+            if event.button == 5:    #마우스휠내리기
+                to_y = +bar_speed #캐릭터를 아래로 캐릭터의 속도만큼 내리기
 
+    if ypos < 0:        #캐릭터가 창을 넘어가려 하면 멈춤
+            ypos = 0
+
+
+    elif ypos > SCREENHEIGHT - bar_height:   #캐릭터가 창을 넘어가려하면 멈춤
+            ypos= SCREENHEIGHT - bar_height
+
+    ypos += to_y * dt  # 캐릭터의 포지션을 y만큼 실제 움직임 프레임수(dt)만큼 곱해서 보정
     #화면지우기
     SCREEN.fill((0, 0, 0))
 
@@ -81,8 +106,8 @@ while play:
     moveNote()
 
     SCREEN.blit(line, (line_x_pos, line_y_pos))
+    SCREEN.blit(real_char, (xpos, ypos))  #캐릭터 그리기
 
     pygame.display.flip()
-    clock.tick(60)    
 
 pygame.quit()
