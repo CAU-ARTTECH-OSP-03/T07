60b6bdb87d3746c2571e643fa4db2685556bb98c
HAN-GISU <hks4939@naver.com>
2021-12-13 21:44:29 +0900

    통합시도

--- 통합시도/통합시도.py
+++ 통합시도/통합시도.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.rect import*
 import random
+import pygame as pg
+import sys
 
 #변수
 play = True
@@ -8,7 +10,14 @@ SCREENHEIGHT = 400
 SCREENWIDTH = 800
 move = Rect(0,0,0,0)
 delay = 0
+score_x=100 #점수판 위치
+score_y=100
 verdict = False #판정 시작 종료를 True False로 했습니다. 스페이스바를 누르면 True로 바뀌고 함수 실행후 Flase
+TITLE = "게임시작, 게임오버"
+FPS = 60
+
+
+
 
 #바 생성
 bar=pygame.image.load("bar.png")  #원본캐릭터 사진 업로드  원하는 경로의 사진을 복사 붙여넣기
@@ -30,6 +39,121 @@ pygame.init()
 
 SCREEN = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
 pygame.display.set_caption('노트랜덤생성')
+
+#점수판 세팅
+score_value=0  #점수판 점수
+font=pygame.font.Font("freesansbold.ttf",30) #점수판 글씨 폰트
+#점수판 생성함수
+def showscore(x, y):
+    score = font.render("score:"+str(score_value), True, "red")
+    SCREEN.blit(score,(x,y))
+
+# 체력바, 게임 스타트, 오버
+
+FONT_NAME = 'arial'
+BLACK = (0,0,0)
+RED = (255,0,0)
+GREEN = (0,255,0)
+WHITE = (255, 255, 255)
+player_health = 300
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode((SCREENWIDTH, SCREENHEIGHT)) # 게임화면 크기 설정
+        pg.display.set_caption(TITLE) # 게임 창의 이름 설정
+        self.clock = pg.time.Clock() # 게임 속도 조절에 사용 (프레임)
+        self.play = True # 게임이 실행중인지 저장
+        self.font_name = pg.font.match_font(FONT_NAME)
+        
+
+
+    def run(self):
+        # Game Loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS) # 게임 프레임 설정
+            self.events()
+            self.draw() 
+
+    def events(self):
+        self.clock.tick(FPS)
+        self.screen.fill(BLACK)
+        global player_health
+
+        pg.draw.rect(self.screen, RED,(150, 450, 300, 10)) #직사각형 만드는 함수 #(X, Y, width, height)
+        pg.draw.rect(self.screen, GREEN,(150, 450, player_health, 10))
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.play = False
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 1: #(마우스 좌클릭) 체력 감소
+               player_health -= 20
+
+            if event.type == pg.MOUSEBUTTONDOWN and event.button == 3: #(마우스 우클릭) 체력 증가
+               player_health += 20
+
+            if player_health > 300:
+                player_health = 300
+
+            if player_health == 0:
+                self.playing = False
+
+            
+
+        
+           
+    def draw(self):
+        pg.display.update() # 화면 업데이트
+        self.screen.fill(BLACK)
+
+
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.play = False
+                if event.type == pg.KEYUP:
+                    waiting = False
+    # 아무키나 눌렀다 때면 waiting이 False가 되고, 그 전까지는 무한루프
+
+
+    def show_start_screen(self):
+        # game splash/start screen
+        self.screen.fill(BLACK)
+        self.draw_text(TITLE, 48, WHITE, SCREENWIDTH/2, SCREENHEIGHT/4)
+        self.draw_text("Arrows to move, Space to jump",
+                       22, WHITE, SCREENWIDTH/2, SCREENHEIGHT/2)
+        self.draw_text("Press any key to play",
+                       22, WHITE, SCREENWIDTH/2, SCREENHEIGHT*3/4)
+        pg.display.flip()
+        self.wait_for_key()
+
+    def show_go_screen(self):
+        # game over/continue
+        if not self.play: 
+            return
+        self.screen.fill(BLACK)
+        self.draw_text("GAME OVER", 48, WHITE, SCREENWIDTH/2, SCREENHEIGHT/4)
+        self.draw_text("Press any key to play again",
+                       22, WHITE, SCREENWIDTH/2, SCREENHEIGHT*3/4)
+        pg.display.flip()
+        self.wait_for_key()
+g = Game()
+g.show_start_screen()
 
 #판정선 생성
 line = pygame.image.load("line.png")
@@ -83,7 +207,8 @@ def moveNote():
 #판정 def
 
 def judge():
-    if verdict:
+    global verdict
+    if verdict == True:
        if 740 > rectNote[i].x > 700: #판정 범위
             if ypos - 50 > rectNote[i].y + 50 > ypos + 210: #노트 높이 = 100, 바높이 = 160 
                rectNote[i].x = -random.randint(607, 809)
@@ -97,9 +222,9 @@ def judge():
     
 #노트 판정 이펙트
 def endNote():
-    if verdict: #노트를 쳤을 때 이펙트
-
-    else: #반응 없이 지나치는 경우 (투명도 조절)
+    global verdict
+ ##   if verdict: #노트를 쳤을 때 이펙트
+    if verdict == False: #반응 없이 지나치는 경우 (투명도 조절)
         global alpha
         rectNoteF.y = rectNote[i].y #시작하는 위치를 맞춰야하는데
         noteF.set_alpha(alpha)
@@ -119,7 +244,12 @@ for i in range(len(note)): #노트생성을 pygame 기본 rect에 저장
 
 #while문
 
+
+
 while play:
+    g.run()
+    player_health = 300
+    g.show_go_screen()
     dt=clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -131,6 +261,14 @@ while play:
                 to_y = +bar_speed #캐릭터를 아래로 캐릭터의 속도만큼 내리기
         if event.type ==pygame.KEYDOWN and event.key ==pygame.K_SPACE: #스페이스바를 누르면 judge함수 실행
             verdict = True
+
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_RIGHT:
+                score_value+=1
+
+            if event.key==pygame.K_LEFT:
+                score_value-=1
+
             
 
 
@@ -147,7 +285,7 @@ while play:
     SCREEN.fill((0, 0, 0))
 
   
-
+    showscore(score_x, score_y)
     makeNote() #노트 생성
     moveNote() #노트 이동
     judge() # 노트 판정
