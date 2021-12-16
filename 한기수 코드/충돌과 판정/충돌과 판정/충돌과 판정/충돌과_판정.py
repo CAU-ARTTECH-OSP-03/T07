e2d4d4ddfc8db76fc04366fae06cf03695f484f5
HAN-GISU <hks4939@naver.com>
2021-12-16 15:53:22 +0900

    충돌과 판정

--- 한기수 코드/충돌과 판정/충돌과 판정/충돌과 판정/충돌과_판정.py
+++ 한기수 코드/충돌과 판정/충돌과 판정/충돌과 판정/충돌과_판정.py
@@ -5,9 +5,7 @@ def Collision():
     if player_health <= 0: ## 게임오버 관련 변수가 나오면 그걸로 수정해주세요
         return
     for rect in rectNote:
-        if rect.y == -1:
-            continue
-        if rect.top < ypos and (ypos + bar_height) < rect.bottom and rect.left < (xpos + 5) and (xpos + 5) < rect.right: # 노트가 bar와 만날 때 체력 20과 점수 100을 깎음
+        if rectNote[i].y + 100 < ypos and (ypos + bar_height) < rectNote[i].y and rectNote[i].x  < (xpos + 5) and (xpos + 5) < rectNote[i].x : # 노트가 bar와 만날 때 체력 20과 점수 100을 깎음
             player_health -= 20 
             score_value -= 100 
 
@@ -23,7 +21,7 @@ verdictBar_ypos = ypos
 ### while문에 들어갈 내용
 
             if event.type ==pygame.KEYDOWN and event.key ==pygame.K_SPACE: #스페이스바를 누를 때 verdictBar와 노트가 겹치면 
-                if rect.top < ypos and (ypos + bar_height) < rect.bottom and rect.left < (verdictBar_xpos + 5) and (verdictBar_xpos + 5) < rect.right:
+                if rectNote[i].y + 100 < ypos and (ypos + bar_height) < rectNote[i].y and rectNote[i].x < (verdictBar_xpos + 5) and (verdictBar_xpos + 5) < rectNote[i].x + 30:
                     rectNote[i].x = -607
                     rectNote[i].y = random.randint(0, SCREENHEIGHT - 100)
                     player_health += 20 
